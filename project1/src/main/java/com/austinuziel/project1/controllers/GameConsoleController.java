7db2f4ab9a1e70f345d871b27d17cc59c525dc2d
Haguermeister <cadethague70@gmail.com>
2022-10-17 19:51:29 -0400

    Added 'red' controllers in order to implement TDD

--- project1/src/main/java/com/austinuziel/project1/controllers/GameConsoleController.java
+++ project1/src/main/java/com/austinuziel/project1/controllers/GameConsoleController.java
@@ -1,9 +1,31 @@
 package com.austinuziel.project1.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.austinuziel.project1.models.GameConsole;
+import com.austinuziel.project1.repositories.GameConsoleRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/GameConsole")
 public class GameConsoleController {
+    @Autowired
+    GameConsoleRepo repo;
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<GameConsole> getGameConsoles() {return new ArrayList<>();}
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public GameConsole addGameConsole(@RequestBody GameConsole gameConsole) {return new GameConsole();}
+
+    @PutMapping()
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateGameConsole(@RequestBody GameConsole gameConsole) {}
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGameConsole(@PathVariable Integer id) {}
 }
