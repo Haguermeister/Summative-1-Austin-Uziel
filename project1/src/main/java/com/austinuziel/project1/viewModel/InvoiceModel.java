d0c7f08fda7541f72659e415dc6a70979704e87d
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-17 20:44:30 -0400

    initial design of invoice class

--- project1/src/main/java/com/austinuziel/project1/viewModel/InvoiceModel.java
+++ project1/src/main/java/com/austinuziel/project1/viewModel/InvoiceModel.java
@@ -1,4 +1,182 @@
 package com.austinuziel.project1.viewModel;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "invoice")
 public class InvoiceModel {
+
+    //    FIELDS
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "invoice_id")
+    private Integer id;
+
+    private String name;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    @Column(name = "zipcode")
+    private int zipCode;
+
+    @Column(name = "item_type")
+    private String itemType;
+
+    @Column(name = "item_id")
+    private int ItemId;
+
+    private int quantity;
+
+    private float unit_price;
+
+    private float subtotal;
+
+    private float tax;
+
+    @Column(name = "processing_fee")
+    private float processingFee;
+
+    private Long total;
+
+    //    CONSTRUCTORS
+    public InvoiceModel() {
+    }
+
+    public InvoiceModel(Integer id, String name, String street, String city, String state, int zipCode, String itemType, int itemId, int quantity, float unit_price, float subtotal, float tax, float processingFee, Long total) {
+        this.id = id;
+        this.name = name;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.itemType = itemType;
+        ItemId = itemId;
+        this.quantity = quantity;
+        this.unit_price = unit_price;
+        this.subtotal = subtotal;
+        this.tax = tax;
+        this.processingFee = processingFee;
+        this.total = total;
+    }
+
+    //    GETTERS AND SETTERS
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public int getItemId() {
+        return ItemId;
+    }
+
+    public void setItemId(int itemId) {
+        ItemId = itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getUnit_price() {
+        return unit_price;
+    }
+
+    public void setUnit_price(float unit_price) {
+        this.unit_price = unit_price;
+    }
+
+    public float getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(float subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public float getTax() {
+        return tax;
+    }
+
+    public void setTax(float tax) {
+        this.tax = tax;
+    }
+
+    public float getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(float processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
 }
