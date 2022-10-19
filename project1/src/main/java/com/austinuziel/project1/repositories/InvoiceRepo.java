7b85bd21d240d280baea22da33eb3b1ca4937bd7
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-19 11:42:23 -0400

    adding test cases for repository invoice

--- project1/src/main/java/com/austinuziel/project1/repositories/InvoiceRepo.java
+++ project1/src/main/java/com/austinuziel/project1/repositories/InvoiceRepo.java
@@ -2,10 +2,11 @@ package com.austinuziel.project1.repositories;
 
 import com.austinuziel.project1.models.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface InvoiceRepo extends JpaRepository<Invoice, Integer> {
 
     List<Invoice> findByName(String name);
