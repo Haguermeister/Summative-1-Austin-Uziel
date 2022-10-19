d1ef7d3eb12f6f5b605a3230295c818d18d54bab
UzielCarranza <uzielcarranzac97@gmail.com>
2022-10-19 12:41:17 -0400

    created custom methods to look for tshirt by size and color

--- project1/src/main/java/com/austinuziel/project1/repositories/TShirtRepo.java
+++ project1/src/main/java/com/austinuziel/project1/repositories/TShirtRepo.java
@@ -4,7 +4,13 @@ import com.austinuziel.project1.models.TShirt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface TShirtRepo extends JpaRepository<TShirt, Integer> {
+    List<TShirt> findByColor(String color);
+
+    List<TShirt> findBySize(String size);
+
 }
